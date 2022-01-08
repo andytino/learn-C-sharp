823be12a0b6038c4ebc163706d59442430ae05e8
Thang Phan <phanvietthang94@gmail.com>
2022-01-08 08:58:31 +0700

    initial commit

---
+++ Program copy.cs
@@ -0,0 +1,50 @@
+﻿// using System;
+
+// namespace HelloWorld
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             int a, b, c;
+
+//             double d, x1, x2;
+//             Console.Write("\n");
+//             Console.Write("Giai phuong trinh bac hai trong C#:\n");
+//             Console.Write("----------------------------------------");
+//             Console.Write("\n\n");
+
+//             Console.Write("Nhap gia tri cua a: ");
+//             a = Convert.ToInt32(Console.ReadLine());
+//             Console.Write("\nNhap gia tri cua b: ");
+//             b = Convert.ToInt32(Console.ReadLine());
+//             Console.Write("\nNhap gia tri cua c: ");
+//             c = Convert.ToInt32(Console.ReadLine());
+
+//             d = b * b - 4 * a * c;
+//             if (d == 0)
+//             {
+//                 Console.Write("Phuong trinh co mot nghiem duy nhat.\n");
+//                 x1 = -b / (2.0 * a);
+//                 x2 = x1;
+//                 Console.Write("Nghiem duy nhat cua phuong trinh la = {0}\n", x1);
+//             }
+//             else if (d > 0)
+//             {
+//                 Console.Write("Phuong trinh co hai nghiem thuc phan biet\n");
+
+//                 x1 = (-b + Math.Sqrt(d)) / (2 * a);
+//                 x2 = (-b - Math.Sqrt(d)) / (2 * a);
+
+//                 Console.Write("Nghiem thu nhat: x1 = {0}\n", x1);
+//                 Console.Write("Nghiem thu nhat: x2 = {0}\n", x2);
+//             }
+//             else
+//                 Console.Write("Phuong trinh vo nghiem (hay khong co nghiem thuc nao)\n\n");
+
+//             Console.ReadKey();
+//         }
+
+
+//     }
+// }
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static bool checkNumber(string? strInput)
+        {
+            double number;
+            if (Double.TryParse(strInput, out number))
+            {
+                Console.WriteLine(number);
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("{0} is outside the range of a Double.", strInput);
+                return false;
+            }
+        }
+        static void Add(string? a, string? label)
+        {
+            bool isNumber = checkNumber(a);
+            if (!isNumber)
+            {
+                Console.Write($"Nhap {label}: ");
+                a = Console.ReadLine();
+                Add(a, "a");
+            }
+        }
+        static void Main(string[] args)
+        {
+            string? a, b;
+            Console.Write("Nhap a: ");
+            a = Console.ReadLine();
+            Add(a, "a");
+            Console.Write("Nhap b: ");
+            b = Console.ReadLine();
+            Add(b, "b");
+
+        }
+    }
+}
